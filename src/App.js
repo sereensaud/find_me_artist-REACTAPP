cd44683868e57f5cccf19f658cf9fe92466d13cf
sereensaud <66210596+sereensaud@users.noreply.github.com>
2022-01-09 17:09:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React APP FOR ARTIST
+          React APP FOR ARTIST!
         </a>
       </header>
     </div>
